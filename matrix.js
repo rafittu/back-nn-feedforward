2d027f13ebc220d9495fdec7847a48665224777e
rafittu <rafittu@outlook.com>
2024-09-28 16:49:45 -0300

    feat: matrix constructor

---
+++ matrix.js
@@ -0,0 +1,16 @@
+class Matrix {
+    constructor(rows, cols){
+        this.rows = rows;
+        this.cols = cols;
+    
+        this.data = [];
+
+        for (let i = 0; i < rows; i++) {
+            let arr = [];
+            for (let j = 0; j < cols; j++) {
+                arr.push(Math.floor(Math.random() * 10));
+            }
+            this.data.push(arr);
+        }
+    }
+}
